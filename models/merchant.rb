66199aa984b0e1baa2efa174e2359d07a745976e
gavinblack87 <gavin_black@hotmail.co.uk>
2019-07-19 14:14:18 +0100

    merchant update

--- models/merchant.rb
+++ models/merchant.rb
@@ -26,6 +26,20 @@ class Merchant
     @id = id.to_i
   end
 
+  def update()
+    sql = "UPDATE merchants
+    SET
+    (
+      merch_name
+    ) =
+    (
+      $1
+    )
+    WHERE id = $2"
+    values = [@merch_name, @id]
+    SqlRunner.run(sql, values)
+  end
+
 
 
 
