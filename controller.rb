19a58c1ba625f86a938f452486dad0600dd28a4b
gavinblack87 <gavin_black@hotmail.co.uk>
2019-07-19 15:50:30 +0100

    controller index

--- controller.rb
+++ controller.rb
@@ -3,3 +3,9 @@ require('sinatra/reloader')
 require_relative('models/merchant.rb')
 require_relative('models/transaction.rb')
 also_reload('./models/*')
+
+
+get '/transactions' do
+  @transactions = Transactions.all
+  erb(:index)
+end
